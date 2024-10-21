5407c2144720609a1c70207ee507a2d9d80d3a3e
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2024-10-21 16:27:24 -0500

    d

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  baseUrl: 'http://localhost:2323/api',
-  //baseUrl: 'http://sv-YaAUgKfBpu.cloud.elastika.pe:2424/api',
+  //baseUrl: 'http://localhost:2323/api',
+  baseUrl: 'https://sv-YaAUgKfBpu.cloud.elastika.pe:2323/api',
   token: 'TOKEN_PREFS',
   tokenExpiry: 'token_expiry',
 };
